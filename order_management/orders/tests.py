52808a92fefb197ecbc745ca7643922267b36b83
Aibek Minbaev <aibekminbaev050402@gmail.com>
2025-03-16 20:19:20 +0900

    done: after deadline; added pytest, connection connectivity test

--- order_management/orders/tests.py
+++ order_management/orders/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+import pytest 
+from django.urls import reverse
+
+
+@pytest.mark.django_db
+def test_database_connection():
+    from django.db import connection
+    try:
+        connection.ensure_connection()
+        assert connection.is_usable() is True
+    except Exception:
+        assert False, "Database connection failed!"
+
